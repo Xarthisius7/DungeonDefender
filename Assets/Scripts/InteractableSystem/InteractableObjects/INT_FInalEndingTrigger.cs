0e11ead8883348ee7df53193906134f81175388f
Tom019 <hidalgo.tomas99@gmail.com>
2024-11-25 12:24:54 -0500

    Endings added (NotSound) + Some minor script changes

--- Assets/Scripts/InteractableSystem/InteractableObjects/INT_FInalEndingTrigger.cs
+++ Assets/Scripts/InteractableSystem/InteractableObjects/INT_FInalEndingTrigger.cs
@@ -4,11 +4,29 @@ using UnityEngine;
 
 public class INT_FInalEndingTrigger : MonoBehaviour, IInteractable
 {
+    int nbLores;
     public void OnInteraction()
     {
         //TODO: TOM : ADD your code that link to the game ending here
+        nbLores = VNSceneManager.Instance.loreLearned;
 
+        if (nbLores < 8)
+        {
+            //EndingA
+            return;
+        }
+            
+        else if (nbLores >= 8 && nbLores < 19)
+        {
+            //EndingB
+            return;
+        }
 
+        else
+        {
+            //EndingC
+            return;
+        }
 
     }
 
